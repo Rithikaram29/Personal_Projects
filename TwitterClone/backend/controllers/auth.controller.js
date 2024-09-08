6606a7905560be3a433413b31bed18b78a113efe
Rithikaram29 <rithikaram293@gmail.com>
2024-09-08 14:58:50 +0530

    project Setup

---
+++ TwitterClone/backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req,res)=>{
+    res.json({
+        data: "you have hit signup endpoint"
+    })
+}
+
+export const login = async (req,res)=>{
+    res.json({
+        data: "you have hit login endpoint"
+    })
+}
+
+export const logout = async (req,res)=>{
+    res.json({
+        data: "you have hit logout endpoint"
+    })
+}
---
+++ TwitterClone/backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+import authRoutes from "./routes/auth.routes.js"
+import connectMongoDB from "./db/connectMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    connectMongoDB()
+})
