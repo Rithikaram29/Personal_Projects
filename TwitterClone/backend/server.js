c45160d48cb7b8a7c5b0a4c3ee650fa01400239b
Rithikaram29 <rithikaram293@gmail.com>
2024-09-09 15:07:33 +0530

    push2

--- TwitterClone/backend/server.js
+++ TwitterClone/backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser'
 
-dotenv.config()
+dotenv.config();
 
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -9,6 +10,9 @@ import connectMongoDB from "./db/connectMongoDB.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
