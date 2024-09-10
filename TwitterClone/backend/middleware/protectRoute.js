04278372b2bb7ccab8e1439c5120eea76eaa7aa2
Rithikaram29 <rithikaram293@gmail.com>
2024-09-10 16:28:45 +0530

    part2 push

--- TwitterClone/backend/middleware/protectRoute.js
+++ TwitterClone/backend/middleware/protectRoute.js
@@ -1,9 +1,9 @@
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 export const protectRoute = async (req,res,next)=>{
     try {
-        const token = req.cookie.jwt;
+        const token = req.cookies.jwt;
         if(!token){
             return res.status(401).json({error:"Unauthorized: No Token provided"})
         }
@@ -14,7 +14,7 @@ export const protectRoute = async (req,res,next)=>{
             return res.status(401).json({error:"Unauthorized: invalid Token"})
         }
 
-        const user = await user.findById(decoded.userId).select("-password");
+        const user = await User.findById(decoded.userId).select("-password");
 
         if(!user){
             return res.status(404).json({error:"User not found"});
