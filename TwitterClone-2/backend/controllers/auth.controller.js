62cd4d5e2f77f40a512d6f1439025902a1683c00
Rithikaram29 <rithikaram293@gmail.com>
2024-09-28 03:21:17 +0530

    43mins done

--- TwitterClone-2/backend/controllers/auth.controller.js
+++ TwitterClone-2/backend/controllers/auth.controller.js
@@ -1,16 +1,69 @@
-export const signup = async (req,res)=>{
-    res.json({
-        data: "you hit the signup endpoint"
-    })
+import User from '../models/model.js';
+import bcrypt from 'bcryptjs'
+
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, email, password } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "invalid email format" });
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ error: "Username already taken" })
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ error: "Email already taken" })
+        }
+
+        //hash password
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            username,
+            email,
+            password: hashedPassword
+
+        })
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                email: newUser.email,
+                following: newUser.following,
+                followers: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg
+            })
+        }else{
+            res.status(500).json({error: "Internal server error"})
+        }
+
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const login = async (req,res)=>{
+export const login = async (req, res) => {
     res.json({
         data: "you hit the login endpoint"
     })
 }
 
-export const logout = async (req,res)=>{
+export const logout = async (req, res) => {
     res.json({
         data: "you hit the logout endpoint"
     })
